207473c969e3c562fd31cd0871cc06eda9b3e27f
toorajj <tooraj@hotmail.com>
2020-12-13 14:51:51 -0800

    npm (init, install, i express ejs method-override) created package.json and package-lock.json, verified dependencies, edited server.js and connected to localhost:3001

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-const expree = reqire("express");
+// External Modules
+const express = require("express");
+
+//Intance Module
+const app = express();
+
+//System Variables
+const PORT = 3001;
+
+//Server Bind
+app.listen(PORT, function(){
+    console.log(`Server is live at http"//localhost:${PORT}/`);
+});
