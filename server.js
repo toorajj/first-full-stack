ab24521064343681e73cb5e47fe025b81cd5bd93
toorajj <tooraj@hotmail.com>
2020-12-13 15:55:21 -0800

    server.js, home.ejs set up for home route

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 // External Modules
 const express = require("express");
+const methodOverRide = require('method-override');
 
 //Intance Module
 const app = express();
 
 //System Variables
 const PORT = 3001;
+
+//View Engine
+app.set('view engine', 'ejs');
+
+//Middleware
+app.use( express.urlencoded({ extended: true }));
+app.use( methodOverRide( '_method'));
+
+//Routes
+app.get('/', (req, res) => res.render('home'));
+
 
 //Server Bind
 app.listen(PORT, function(){
