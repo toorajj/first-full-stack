125a65db2a45b3dde8de34fd3853ebbe329e6e88
toorajj <tooraj@hotmail.com>
2020-12-14 02:08:04 -0800

    edited views and controllers

--- controllers/cars.js
+++ controllers/cars.js
@@ -1,20 +1,19 @@
-const { response } = require('express');
 const express = require('express');
 
 const router = express.Router();
 
 const db = require('../models');
 
-router.get("/", function (request, response) {
+router.get("/", function (req, res) {
     db.Car.find({}, function (error, allCar) {
         if (error) {
             console.log(error);
-            return response.send("Internal Server Error");
+            return res.send("Internal Server Error");
         } else {
             const context = {
                 cars: allCar,
             };
-            return response.render("cars/index", context);
+            return res.render("cars/index", context);
         }
     });
 });
@@ -23,10 +22,16 @@ router.get("/", function (request, response) {
 
 router.get('/new', (req, res) => res.render('cars/new'));
 
-router.post('/addcar', (req, res)=> {
-    req.body._id = db.Cars.length;
-    db.Cars.push(req.body);
-    res.redirect('/cars');
+router.post('/addCar', (req, res) => {
+    db.Car.create(req.body, function (error, createdCar) {
+        if (error) {
+            console.log(error);
+            return res.send("Internal Server Error");
+        } else {
+            console.log("Added Car", createdCar);
+            return res.redirect("/cars");
+        }
+    });
 });
 
 
